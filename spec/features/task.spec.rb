fc999a5d68c908ea20525e330398b44d27ceda00
Decosan <junx0401@gmail.com>
2019-08-02 18:13:12 +0900

    step11

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -3,11 +3,18 @@ require 'rails_helper'
 
 # このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
+  background do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+
+    # backgroundの中に記載された記述は、そのカテゴリ内（feature "タスク管理機能", type: :feature do から endまでの内部）
+    # に存在する全ての処理内（scenario内）で実行される
+    # （「タスク一覧のテスト」でも「タスクが作成日時の降順に並んでいるかのテスト」でも、background内のコードが実行される）
+    Task.create!(title: 'test_task_01', content: 'testtesttest')
+    Task.create!(title: 'test_task_02', content: 'mofmofmofmof')
+  end
     # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
   scenario "タスク一覧のテスト" do
     # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    Task.create!(title: 'test_task_01', content: 'testtesttest')
-    Task.create!(title: 'test_task_02', content: 'samplesample')
 
     # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
@@ -31,12 +38,15 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク詳細のテスト" do
-    Task.create!(title: 'test_task_03', content: 'testtesttest2')
-
+    
     visit tasks_path
     click_on 'Detail'
     # save_and_open_page
 
-    expect(page).to have_content 'testtesttest2'
+    expect(page).to have_content 'testtesttest'
+  end
+
+  scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+    # ここにテスト内容を記載する
   end
 end
