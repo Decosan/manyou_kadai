53e0e3585de0de8559efe7cfa7bbc89f7cfb913b
Decosan <junx0401@gmail.com>
2019-08-02 16:23:53 +0900

    step10

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Manyou
     # config.i18n.default_locale = :en
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
