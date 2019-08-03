5e458f326ccafdc938e659b7b7737ee01d6c9081
Decosan <junx0401@gmail.com>
2019-08-03 15:24:07 +0900

    Not Null制約追加

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,6 +1,22 @@
 # このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
 require 'rails_helper'
 
+RSpec.describe Task, type: :model do
+
+  it "titleが空ならバリデーションが通らない" do
+    task = Task.new(title: '', content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    # ここに内容を記載する
+  end
+
+  it "titleとcontentに内容が記載されていればバリデーションが通る" do
+    # ここに内容を記載する
+  end
+end
+
 # このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
   
