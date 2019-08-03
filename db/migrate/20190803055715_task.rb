60a39c9e76912e485e90bb2d598818a5c29c3759
Decosan <junx0401@gmail.com>
2019-08-03 17:31:20 +0900

    Add:Taskモデルバリデーションを設定

--- db/migrate/20190803055715_task.rb
+++
@@ -1,6 +0,0 @@
-class Task < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :title, :string, null: false
-    change_column :tasks, :content, :string, null: false
-  end
-end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -9,11 +9,13 @@ RSpec.describe Task, type: :model do
   end
 
   it "contentが空ならバリデーションが通らない" do
-    # ここに内容を記載する
+    task2 = Task.new(title: '仮タイトル', content: '')
+    expect(task2).not_to be_valid
   end
 
   it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    # ここに内容を記載する
+    task3 = Task.new(title: '仮タイトル', content: '成功テスト')
+    expect(task3).to be_valid
   end
 end
 
