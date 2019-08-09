dec28b810e3056cbe5a6fa04c47f8961d8f9a183
Decosan <junx0401@gmail.com>
2019-08-09 15:29:05 +0900

    UserとTaskno 紐付け実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation {email.downcase!}
+  has_many :tasks
 
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },uniqueness: true,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_044529) do
+ActiveRecord::Schema.define(version: 2019_08_09_061923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2019_08_09_044529) do
     t.date "sort_expired"
     t.string "status"
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_08_09_044529) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
