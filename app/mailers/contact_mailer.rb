4aecaaeeba299678e8fcc825c58733cb77fc1f2b
Decosan <junx0401@gmail.com>
2019-10-27 11:57:49 +0900

    heroku deploy01-2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(user)
     @user = user
 
-    mail to: "junx0401@gmail.com", subject: "期限が間近となっているタスクのお知らせ"
+    mail to: user.email, subject: "期限が間近となっているタスクのお知らせ"
   end
 end
